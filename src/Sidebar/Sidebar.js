f53c9c310d50ea2c762c0c5c2ce02ecf57f12d14
Dhivya <divbaluu@gmail.com>
2021-01-28 22:07:08 +0530

    logo issue in deployment fixed

--- src/Sidebar/Sidebar.js
+++ src/Sidebar/Sidebar.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import { useLocation } from "react-router-dom";
+import logo from "../assets/music-logo.svg";
 import "./Sidebar.scss";
 
 const Sidebar = () => {
@@ -10,7 +11,7 @@ const Sidebar = () => {
     <div className="sidebar">
       <div className="sidebar-desktop">
         <div className="flex-container align-items-center">
-          <img className="logo-image" alt="logo" src="/music-logo.svg" />
+          <img className="logo-image" alt="logo" src={logo} />
           <span className="company-title">VBI Music</span>
         </div>
         <ul className="nav-links">
